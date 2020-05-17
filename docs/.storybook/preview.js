23777cb4bf7be9239d6c827855fff639ef3cbda3
Wasim Almadfaa <wm.madfaa@gmail.com>
2020-05-17 16:26:03 +0200

    setup docs

---
+++ docs/.storybook/preview.js
@@ -0,0 +1,23 @@
+import { addParameters } from "@storybook/html";
+
+const SOURCE_REGEX = /^\(\) => [`'"](.*)['`"]$/;
+
+addParameters({
+  a11y: {
+    config: {},
+    options: {
+      checks: { "color-contrast": { options: { noScroll: true } } },
+      restoreScroll: true,
+    },
+  },
+  options: {
+    showRoots: true,
+  },
+  docs: {
+    iframeHeight: "200px",
+    transformSource: (src) => {
+      const match = SOURCE_REGEX.exec(src);
+      return match ? match[1] : src;
+    },
+  },
+});
